49085961ec95a2b9871087873a7ffc85810da977
BurntRanch <69512353+BurntRanch@users.noreply.github.com>
2023-01-09 15:20:42 +0300

    legacy packs

--- decrypt.py
+++ decrypt.py
@@ -19,42 +19,19 @@ with open(ecd_file, 'rb') as file:
     while True:
         file.seek(stat(ecd_file).st_size - 6)
         format = file.read(6)
-        if format == b'ECD1.0':
-            # read the headers at the end
-            file.seek(stat(ecd_file).st_size - 70)
-            # get the salt (16 bytes read, we are now offsetted to the IV)
-            ekey = pbkdf2_hmac('SHA256', sha256(input("Insert encryption key: ").encode()).digest(), file.read(16), 400000, 32)
-            # get the IV (we are now offsetted to the hash)
-            aes_obj = new(ekey, MODE_CBC, file.read(16))
-            # reset the offset
-            file.seek(0)
-            # read the content (content len = file length - header overhead size (16 + 16 + 32 + 6 = 70))
-            content = aes_obj.decrypt(file.read(stat(ecd_file).st_size - 70))
-            # seek to the last 32 bytes of the header (hash)
-            file.seek(stat(ecd_file).st_size - 38)
-            # verify hash with content digest for password verification
-            if sha256(content).digest() != file.read(32):
-                print("Incorrect password!")
-                continue
-        elif format == b'ECD1.1':
-            length = 102
-            # read the headers at the end
-            file.seek(stat(ecd_file).st_size - length)
-            # get the salt (16 bytes read, we are now offsetted to the IV)
-            ekey = pbkdf2_hmac('SHA256', sha256(input("Insert encryption key: ").encode()).digest(), file.read(16), 400000, 32)
-            # get the IV (we are now offsetted to the hash)
-            aes_obj = new(ekey, MODE_CBC, file.read(16))
-            # reset the offset
-            file.seek(0)
-            # read the content (content len = file length - header overhead size (16 + 16 + 32 + 6 = 70))
-            content = aes_obj.decrypt(file.read(stat(ecd_file).st_size - length))
-            # seek to the last 32 bytes of the header (hash)
-            file.seek(stat(ecd_file).st_size - (length - 32))
-            # verify hash with content digest for password verification
-            if sha256(content).digest() != file.read(32) or sha256(content[:32]).digest() != file.read(32):
-                print("Incorrect password!")
-                continue
-        elif format == b'ECD1.2':
+        if format != b'ECD1.2':
+            if format.startswith('ECD'):
+                print('Loading Legacy Pack..')
+                try:
+                    exec(f'from {format.decode().replace(".", "_")} import process')
+                    content = process(ecd_file, file)
+                    if content == False:
+                        continue
+                except ImportError:
+                    print(f'You do not have a Legacy Pack for format version {format.decode()}, Please download it from https://github.com/BurntRanch/DirEncrypt/releases/tag/{format.decode()}-LP.')
+            else:
+                print('Unknown format!')
+        else:
             length = 134
             # read the headers at the end
             file.seek(stat(ecd_file).st_size - length)
@@ -72,8 +49,6 @@ with open(ecd_file, 'rb') as file:
             if sha256(content).digest() != file.read(32) or sha256(content[32:]).digest() != file.read(32) or sha256(content[:32]).digest() != file.read(32):
                 print("Incorrect password!")
                 continue
-        else:
-            print('Unknown format!')
         break
 
 if content:
